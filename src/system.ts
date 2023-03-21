7ee7c543656285f4ed9b91e49ca063ac27d7ab98
Littlee <104838409+10fantastic@users.noreply.github.com>
2023-03-21 21:04:12 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -8,6 +8,7 @@ export const setting = {
 }
 export const message = `- 学无止境
 - 个人专享-速度更快-    有问题请联系爱你的ZJM
+- 每次对话消耗一个爱心，多爱我一点就可以无限使用哦~
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
