6c33402d8c2f928263dd01f84d4e1e08ca3a61df
Littlee <104838409+10fantastic@users.noreply.github.com>
2023-03-21 20:23:25 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -1,5 +1,11 @@
-
-
+export const setting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: ""
+}
 export const message = `- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
