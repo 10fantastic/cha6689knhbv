76301a35e0e13046400c8ffba1ec24974cf9d61f
Littlee <104838409+10fantastic@users.noreply.github.com>
2023-03-21 20:41:42 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -6,7 +6,8 @@ export const setting = {
   password: "",
   systemRule: ""
 }
-export const message = `个人专享-速度更快-    有问题请联系爱你的ZJM
+export const message = `- 学无止境
+- 个人专享-速度更快-    有问题请联系爱你的ZJM
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
