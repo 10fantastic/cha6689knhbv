63da14b1d4396af8d79dfd2aa336a45018335346
Littlee <104838409+10fantastic@users.noreply.github.com>
2023-03-24 11:54:29 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -2,14 +2,13 @@ export const setting = {
   continuousDialogue: true,
   archiveSession: false,
   openaiAPIKey: "",
-  openaiAPITemperature: 60,
+  openaiAPITemperature: 50,
   password: "",
   systemRule: ""
 }
 export const message = `- 学无止境
 - 个人专享-速度更快-    有问题请联系爱你的ZJM
-- 每次对话消耗一个爱心，多爱我一点就可以无限使用哦~
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+- 每次对话消耗一个爱心，多爱我一点就可以无限使用哦~`
 
 export type Setting = typeof setting
 
